7d86674ea9e41ec7b8fff43e7bef4bcb350b129e
PhilipeeX <dev.philipe@outlook.com>
2023-06-24 06:30:46 -0300

    feat: 'didade' para 'idade'

--- nome_idade.rb
+++ nome_idade.rb
@@ -2,4 +2,4 @@ puts "Digite seu nome"
 nome = gets.chomp
 puts "Agora digite sua idade"
 idade = gets.chomp
-puts "Bem vindo #{nome}, com didade de #{idade} anos"
+puts "Bem vindo #{nome}, com idade de #{idade} anos"
