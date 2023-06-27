17639fdda4da132d573553aa69bedc6ff1c00713
Philipe <dev.philipe@outlook.com>
2023-06-27 12:22:12 -0300

    fix(aula10): attempt to build a translator using an API

--- projeto_final/tradutor_de_texto.rb
+++ projeto_final/tradutor_de_texto.rb
@@ -1,10 +1,17 @@
-require 'google/cloud/translate'
+require "google/cloud/translate"
 
-translation_client = Google::Cloud::Translate.translation_service
+# Inicialize o cliente usando a chave de API
+client = Google::Cloud::Translate.translation_service(credentials: "____INSIRA_A_API_AQUI")
 
-result = translation_client.translate_text contents: ["Hello, world!"],
-                                           mime_type: "text/plain",
-                                           source_language_code: "en-US",
-                                           target_language_code: "ja-JP",
-                                           parent: "projects/my-project-name"
-puts result.translations.first.translated_text
+texto_original = "Hello, world!"
+
+# Traduza o texto para um idioma específico
+traducao = client.translate_text(
+  parent: "projects/chrome-entropy-391113/locations/global",
+  contents: [texto_original],
+  target_language_code: "pt"
+)
+
+# Exiba o resultado
+puts "Texto original: #{texto_original}"
+puts "Tradução para o português: #{traducao.translations[0].translated_text}"
