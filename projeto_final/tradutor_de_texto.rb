cca1d1232dcdd14e989bf69994b3c5b066c4a566
Philipe <dev.philipe@outlook.com>
2023-06-28 19:24:11 -0300

    feat(traduto_de_texto): class created and extracting file to root directory

--- projeto_final/tradutor_de_texto.rb
+++ projeto_final/tradutor_de_texto.rb
@@ -1,26 +1,33 @@
 require "google/cloud/translate"
+require 'time'
+class MyTextTranslate
+  def self.translate_text(text, target_language)
+    project_id = "chrome-entropy-391113"
+    credentials_path = "./chrome-entropy-391113-cabcfcf8dc52.json"
 
-def translate_text(text, target_language)
-  project_id = "chrome-entropy-391113"
-  credentials_path = "./chrome-entropy-391113-45171ce5e425.json"
+    translate = Google::Cloud::Translate.translation_v2_service project_id: project_id, credentials: credentials_path
+    translation = translate.translate text, to: target_language
+    translation.text
+  rescue => e
+    puts "Ocorreu um erro na tradução: #{e.message}"
+  end
 
-  translate = Google::Cloud::Translate.translation_v2_service project_id: project_id, credentials: credentials_path
-  translation = translate.translate text, to: target_language
-  translation.text
-rescue => e
-  puts "Ocorreu um erro na tradução: #{e.message}"
+  def self.extract_text(text_one, second_text, language)
+    current_time = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    File.open(current_time, 'w') do |arquivo|
+      arquivo.puts "#{text_one} => #{language} => #{second_text}"
+    end
+  end
+  def self.user_iteraction
+    puts 'Digite o texto que deseja traduzir:'
+    text = gets.chomp
+    puts 'Digite a linguagem que o texto será traduzido, ex: Inglês: "en" Espanhol: "es" Francês: "fr" Alemão: "de" Italiano: "it", veja mais em https://cloud.google.com/translate/docs/languages'
+    target_language = gets.chomp
+    extract_text(text, MyTextTranslate.translate_text(text, target_language), target_language)
+    puts MyTextTranslate.translate_text(text, target_language)
+  end
 end
 
-denovo = 'sim'
-while denovo == 'sim'
-  puts 'Digite o texto que deseja traduzir:'
-  text = gets.chomp
-  puts 'Digite a linguagem que o texto será traduzido, ex: Inglês: "en" Espanhol: "es" Francês: "fr" Alemão: "de" Italiano: "it", veja mais em https://cloud.google.com/translate/docs/languages'
-  target_language = gets.chomp
-
-  puts translate_text(text, target_language)
-  puts 'Deseja fazer outra tradução? digite["sim" ou "nao"]'
-  denovo = gets.chomp
-end
+MyTextTranslate.user_iteraction
 
 
